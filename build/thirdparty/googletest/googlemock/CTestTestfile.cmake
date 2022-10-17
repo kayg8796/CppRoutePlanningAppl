9359da69cfd7e4c1499b56988b7cdcef9b62aea2
kayg8796 <litombeg@gmail.com>
2022-10-17 20:09:06 +0200

    fix gitignore

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ngomba/udacity/project1/CppRoutePlanningAppl/thirdparty/googletest/googlemock
-# Build directory: /home/ngomba/udacity/project1/CppRoutePlanningAppl/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ngomba/udacity/project1/CppRoutePlanningAppl/thirdparty/googletest/googletest
-# Build directory: /home/ngomba/udacity/project1/CppRoutePlanningAppl/build/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
